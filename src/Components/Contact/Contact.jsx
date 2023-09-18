f1bd567979d0514912a204fe4d95c9582cfba9f4
yaacinediop <yacinediopkebe@gmail.com>
2023-09-18 13:31:52 +0000

    first commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
 
 const Contact = () => {
   return (
-    <div>
-                                  <h3>Découvrez  prochaine session de créativité.</h3>
+   <div className="container-fluid">
+    <div className="row">
+      <div className="col-lg-3">
+      {/* <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d123485.26331293212!2d-17.551063922539495!3d14.717426940172363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0xec173659c6c7d3f%3A0xd852bd50a2d6744d!2s9727%20Rue%20SC%2092%2C%20Dakar!3m2!1d14.7174646!2d-17.4686164!5e0!3m2!1sfr!2ssn!4v1695042373233!5m2!1sfr!2ssn" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+      </div>
     </div>
+
+    <div className="row">
+      <div className="col-lg-9">
+      
+      </div>
+    </div>
+   </div>
   )
 }
 
--- src/Components/Propos/Propos.jsx
+++ src/Components/Propos/Propos.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import p1 from '../Assets/p1.png';
 import p2 from '../Assets/p2.png'; 
 import p3 from '../Assets/p3.png';
-import p4 from '../Assets/p4.png'
+import p4 from '../Assets/p4.png';
+import p5 from '../Assets/p5.png';
+import {AiOutlineArrowRight} from "react-icons/ai";
+import './Cardprod.css'
 const Propos = () => {
   return (
-    <div className='container-fluid'>
+    <div className='container-fluid '>
       <div className="row">
         <div className="col-lg-6">
           <img src={p1} alt="" className='img-fluid'/>
         </div>
-        <div className="  col-lg-6 justify-content-center">
+        <div className="  col-lg-6 justify-content-center mab">
           <h1>Qui sommes nous ?</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Iste nulla voluptates, illo similique porro enim <br /> corporis cum omnis doloribus in, blanditiis, laboriosam <br /> a nesciunt. Dolorum perferendis quae ullam numquam illum!</p>
           <br />
-          <p className='text-danger'>En savoir plus</p>
+          <p className='text-danger'>En savoir plus <AiOutlineArrowRight /> </p>
 
         </div>
 
@@ -22,45 +25,49 @@ const Propos = () => {
 
       <div className="row">
         
-        <div className="  col-lg-6 justify-content-center">
-          <h1>Qui sommes nous ?</h1>
+        <div className="  col-lg-6 justify-content-center mab">
+          <h1>Notre vision</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Iste nulla voluptates, illo similique porro enim <br /> corporis cum omnis doloribus in, blanditiis, laboriosam <br /> a nesciunt. Dolorum perferendis quae ullam numquam illum!</p>
           <br />
-          <p className='text-danger'>En savoir plus</p>
+          <p className='text-danger'>En savoir plus <AiOutlineArrowRight /></p>
 
         </div>
         <div className="col-lg-6">
           <img src={p2} alt="" className='img-fluid'/>
         </div>
       </div>
-      <br /><br />
 
       <div className="row">
         <div className="col-lg-6">
           <img src={p3} alt="" className='img-fluid'/>
         </div>
-        <div className="  col-lg-6 justify-content-center">
-          <h1>Qui sommes nous ?</h1>
+        <div className="  col-lg-6 justify-content-center mab">
+          <h1>Notre mission</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Iste nulla voluptates, illo similique porro enim <br /> corporis cum omnis doloribus in, blanditiis, laboriosam <br /> a nesciunt. Dolorum perferendis quae ullam numquam illum!</p>
           <br />
-          <p className='text-danger'>En savoir plus</p>
+          <p className='text-danger'>En savoir plus <AiOutlineArrowRight /></p>
 
         </div>
 
       </div>
-       <br /><br />
       <div className="row">
+       
+        <div className="  col-lg-6 justify-content-center mab">
+          <h1>Notre valeur</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Iste nulla voluptates, illo similique porro enim <br /> corporis cum omnis doloribus in, blanditiis, laboriosam <br /> a nesciunt. Dolorum perferendis quae ullam numquam illum!</p>
+          <br />
+          <p className='text-danger'>En savoir plus <AiOutlineArrowRight /></p>
+
+        </div>
         <div className="col-lg-6">
           <img src={p4} alt="" className='img-fluid'/>
         </div>
-        <div className="  col-lg-6 justify-content-center">
-          <h1>Qui sommes nous ?</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Iste nulla voluptates, illo similique porro enim <br /> corporis cum omnis doloribus in, blanditiis, laboriosam <br /> a nesciunt. Dolorum perferendis quae ullam numquam illum!</p>
-          <br />
-          <p className='text-danger'>En savoir plus</p>
-
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <img src={p5} alt="" className='img-fluid'/>
         </div>
-
+       
       </div>
     </div>
   )
